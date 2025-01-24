c39f3e844f37336fe344133641a6175c86beb526
CKtrace <brandonkimwork@gmail.com>
2025-01-24 22:34:08 +0900

    DP Update

---
+++ DP/2xn_타일링2.py
@@ -0,0 +1,12 @@
+N = int(input())
+dp_li = [0] * (N+1)
+
+for i in range(1, N+1):
+    if i == 1:
+        dp_li[i] = 1
+    if i == 2:
+        dp_li[i] = 3
+    if i >= 3:
+        dp_li[i] = dp_li[i-1] + dp_li[i-2]*2
+
+print(dp_li[N] % 10007)
